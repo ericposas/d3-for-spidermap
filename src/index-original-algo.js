55189bb718f42a0793ec510b9750ff8564583441
Eric Posas <web_dev@tutanota.com>
2019-10-29 18:18:44 -0500

    mapping for origins and origins-to-destinations rough draft code working; need to draw the bezier curve line to each destination now

---
+++ src/index-original-algo.js
@@ -0,0 +1,77 @@
+import * as d3 from 'd3'
+import random from 'random'
+import axios from 'axios'
+import _ from 'lodash'
+import './airports.json'
+import './index.scss'
+
+let svgArea = 600
+let svgMargin = 100
+document.getElementById('root').innerHTML += `
+  <svg id='svg-container' width=${svgArea} height=${svgArea} style='background-color:#ccc'></svg>
+`
+let origins
+let destinations = {}
+let size = 4
+const getJSON = async () => {
+  try {
+    let data = await axios.get('./json/airports.json')
+    let randomize = _.shuffle(data.data)
+    origins = randomize.slice(0, 5)
+    // set random set of destinations per origin
+    origins.forEach(o => {
+      destinations[o.code ? o.code : o.four_digit_code] = randomize.slice(0, random.int(10, 20))
+    })
+    console.log(origins)
+    console.log(destinations)
+    run()
+  } catch (e) { console.log(e) }
+}
+
+const run = () => {
+  let svg = document.getElementById('svg-container')
+  let _x = origins.map(d => d.longitude)
+  let _y = origins.map(d => d.latitude) //{ return d.latitude < 0 ? (d.latitude * -1) : (d.latitude * 2) })
+  // sort values to figure out the greatest among them
+  let _xSorted = _x.sort((a,b) => a - b)
+  let _ySorted = _y.sort((a,b) => b - a) //.map(d => d * -1)
+  // console.log(_xSorted, _ySorted)
+  // apply linearScales to our data to ensure that it fits our graph model
+  let originScaleX = d3.scaleLinear()
+                             .domain([_xSorted[0], _xSorted[_xSorted.length-1]])
+                             .range([svgMargin, svgArea - svgMargin])
+  // console.log(_xSorted[0], _xSorted[_xSorted.length-1], _ySorted[0], _ySorted[_ySorted.length-1])
+  let originScaleY = d3.scaleLinear()
+                             .domain([_ySorted[0], _ySorted[_ySorted.length-1]])
+                             .range([svgMargin, svgArea - svgMargin])
+
+  // let olat = originScaleX(d.latitude)
+  // let olong = originScaleY(d.longitude)
+  let olats = origins.map(d => originScaleY(d.latitude)) // y is lat, x is long
+  let olongs = origins.map(d => originScaleX(d.longitude))
+  origins.forEach((d,i) => {
+    svg.innerHTML += `
+      <!--
+      <g>
+        <circle cx=${400/2} cy=${400/2} r=${(400-4)/2} stroke='#000' fill='none'></circle>
+      </g>
+      -->
+      <g>
+        <circle cx=${olongs[i]} cy=${olats[i]} r=${size/2} fill='#000'></circle>
+        <text x=${olongs[i] - size} y=${olats[i] - (size*5)} font-family='arial' font-size='0.5rem' fill='#000'>
+          ${d.code ? d.code : d.four_digit_code}
+        </text>
+        <text x=${olongs[i] - size} y=${olats[i] - (size * 3)} font-family='arial' font-size='0.5rem' fill='#000'>
+          lat: ${parseFloat(d.latitude).toFixed(2)}
+        </text>
+        <text x=${olongs[i] - size} y=${olats[i] - size} font-family='arial' font-size='0.5rem' fill='#000'>
+          long:${parseFloat(d.longitude).toFixed(2)}
+        </text>
+      </g>
+    `
+  })
+
+
+}
+
+getJSON()
--- src/index.js
+++ src/index.js
@@ -5,81 +5,91 @@ import _ from 'lodash'
 import './airports.json'
 import './index.scss'
 
-
-let svgArea = 500
-let svgMargin = 40
+let svgArea = 600
+let svgMargin = 100
 document.getElementById('root').innerHTML += `
   <svg id='svg-container' width=${svgArea} height=${svgArea} style='background-color:#ccc'></svg>
 `
+let svg = document.getElementById('svg-container')
+let size = 4 // location circle/dot size
+let origins = []
+let originsToDestinations = {} // destinations will be arrays inside the originsToDestinations object
+// apply linearScales to our data to ensure that it fits our graph model
+// will be useful to create a UI slider that can adjust the range and domain dynamically
+let linearScaleX, linearScaleY
+let drawnLocations = []
+let xDomainMin = [-110, -70], xDomainExpanded = [-180, -20]
+let yDomainMin = [40, 20], yDomainExpanded = [150, 0]
+let xDomain = xDomainMin, yDomain = yDomainMin
 
-let origins
-let destinations = {}
-let size = 4
-const getJSON = async () => {
+const getDataAndSetup = async () => {
   try {
     let data = await axios.get('./json/airports.json')
-    let randomize = _.shuffle(data.data)
-    origins = randomize.slice(0, 5)
+    origins = _.shuffle(data.data).slice(0, 5) // assign 5 random origins
     // set random set of destinations per origin
-    origins.forEach(o => {
-      destinations[o.code ? o.code : o.four_digit_code] = randomize.slice(0, random.int(10, 20))
+    origins.forEach(d => {
+      let randomDestinations = _.shuffle(data.data).slice(0, random.int(1, 6))
+      originsToDestinations[d.code] = randomDestinations
+      // if () {xDomain = xDomainExpanded} else {xDomain = xDomainMin}
+
     })
-    console.log(origins)
-    console.log(destinations)
-    run()
+    console.log(origins, originsToDestinations)
+    linearScaleX = d3.scaleLinear()
+                               .domain(xDomain)
+                               .range([svgMargin, svgArea - svgMargin])
+    linearScaleY = d3.scaleLinear()
+                               .domain(yDomain)
+                               .range([svgMargin, svgArea - svgMargin])
   } catch (e) { console.log(e) }
 }
 
-const run = () => {
-  let svg = document.getElementById('svg-container')
-  let _x = origins.map(d => d.longitude)
-  let _y = origins.map(d => d.latitude) //{ return d.latitude < 0 ? (d.latitude * -1) : (d.latitude * 2) })
-  // sort values to figure out the greatest among them
-  let _xSorted = _x.sort((a,b) => a - b)
-  let _ySorted = _y.sort((a,b) => b - a) //.map(d => d * -1)
-  console.log(
-    _xSorted,
-    _ySorted
-  )
-  // apply linearScales to our data to ensure that it fits our graph model
-  let originScaleX = d3.scaleLinear()
-                             .domain([_xSorted[0], _xSorted[_xSorted.length-1]])
-                             .range([svgMargin, svgArea - svgMargin])
-  console.log(
-    _xSorted[0], _xSorted[_xSorted.length-1],
-    _ySorted[0], _ySorted[_ySorted.length-1],
-  )
-  let originScaleY = d3.scaleLinear()
-                             .domain([_ySorted[0], _ySorted[_ySorted.length-1]])
-                             .range([svgMargin, svgArea - svgMargin])
-
-  // let olat = originScaleX(d.latitude)
-  // let olong = originScaleY(d.longitude)
-  let olats = origins.map(d => originScaleY(d.latitude)) // y is lat, x is long
-  let olongs = origins.map(d => originScaleX(d.longitude))
-  origins.forEach((d,i) => {
+const skiMap = async () => {
+  await getDataAndSetup()
+  Object.keys(originsToDestinations).forEach((origin, o) => {
+    // draw origin in different style so we can line to and from them later
+    let oX = linearScaleX(origins[o].longitude)
+    let oY = linearScaleY(origins[o].latitude)
+    // let tX = oX - (size*2)
+    // let tY = oY - (size*3.5)
     svg.innerHTML += `
-      <!--
       <g>
-        <circle cx=${400/2} cy=${400/2} r=${(400-4)/2} stroke='#000' fill='none'></circle>
-      </g>
-      -->
-      <g>
-        <circle cx=${olongs[i]} cy=${olats[i]} r=${size/2} fill='#000'></circle>
-        <text x=${olongs[i] - size} y=${olats[i] - (size*5)} font-family='arial' font-size='0.5rem' fill='#000'>
-          ${d.code ? d.code : d.four_digit_code}
-        </text>
-        <text x=${olongs[i] - size} y=${olats[i] - (size * 3)} font-family='arial' font-size='0.5rem' fill='#000'>
-          lat: ${parseFloat(d.latitude).toFixed(2)}
-        </text>
-        <text x=${olongs[i] - size} y=${olats[i] - size} font-family='arial' font-size='0.5rem' fill='#000'>
-          long:${parseFloat(d.longitude).toFixed(2)}
-        </text>
+        <circle cx=${oX} cy=${oY} r=10 fill='none' stroke='red'></circle>
       </g>
     `
+    // <!-- <text x=${tX} y=${tY} font-family='arial' font-size='0.5rem' fill='red'>${origins[o].code}</text> -->
+    // console.log(originsToDestinations[dest])
+    let _x = originsToDestinations[origin].map(d => d.longitude)
+    let _y = originsToDestinations[origin].map(d => d.latitude)
+    // console.log(_y)
+    // sort values to figure out the greatest among them
+    let _xSorted = _x.sort((a,b) => a - b)
+    let _ySorted = _y.sort((a,b) => a - b)
+    let olats = originsToDestinations[origin].map(d => linearScaleY(d.latitude))
+    let olongs = originsToDestinations[origin].map(d => linearScaleX(d.longitude))
+    // console.log(originsToDestinations[origin][i])
+    // plot each destination for each origin location
+    olats.forEach((l, i) => {
+      console.log(_.includes(drawnLocations, originsToDestinations[origin][i].code))
+      if (!_.includes(drawnLocations, originsToDestinations[origin][i].code)) {
+        drawnLocations = drawnLocations.concat(originsToDestinations[origin][i].code)
+        svg.innerHTML += `
+          <g>
+            <circle cx=${olongs[i]} cy=${olats[i]} r=${size/2} fill='#000'></circle>
+            <text x=${olongs[i] - size} y=${olats[i] - (size*5)} font-family='arial' font-size='0.5rem' fill='#000'>
+              ${originsToDestinations[origin][i].code ? originsToDestinations[origin][i].code : originsToDestinations[origin][i].icao}
+            </text>
+            <text x=${olongs[i] - size} y=${olats[i] - (size * 3)} font-family='arial' font-size='0.5rem' fill='#000'>
+              lat: ${parseFloat(originsToDestinations[origin][i].latitude).toFixed(2)}
+            </text>
+            <text x=${olongs[i] - size} y=${olats[i] - size} font-family='arial' font-size='0.5rem' fill='#000'>
+              long:${parseFloat(originsToDestinations[origin][i].longitude).toFixed(2)}
+            </text>
+          </g>
+        `
+      }
+      console.log(drawnLocations)
+    })
   })
-
-
 }
 
-getJSON()
+skiMap()

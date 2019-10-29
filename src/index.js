a374408a4c222d8cd33bbe4bf17a7b57a09d220e
Eric Posas <web_dev@tutanota.com>
2019-10-28 23:28:04 -0500

    added random destinations per origin; now we need to map each destination that resides within the same region one on top of the other, neatly

--- src/index.js
+++ src/index.js
@@ -7,25 +7,33 @@ import './index.scss'
 
 // let svgSize = innerWidth * .65
 let w = innerWidth * .75, h = w
-let upperLimit = (w * .2), lowerLimit = w - (w * .2)
+let originLowerLimitX = (w * .2), originUpperLimitX = (w * .5)
+let originLowerLimitY = (w * .05), originUpperLimitY = (w * .2)
+
+let destLowerLimit = (w * .2), destUpperLimit = w - (w * .2)
 document.getElementById('root').innerHTML += `
   <svg id='svg-container' width=${w} height=${h} style='background-color:#ccc'></svg>
 `
 
 let origins
-let destinations
+let destinations = {}
 let size = 4
 const getJSON = async () => {
   try {
     let data = await axios.get('./json/airports.json')
-    let scrambled = _.shuffle(data.data)
-    origins = scrambled.slice(0, 5)
+    let randomize = _.shuffle(data.data)
+    origins = randomize.slice(0, 5)
+    // set random set of destinations per origin
+    origins.forEach(o => {
+      destinations[o.code ? o.code : o.four_digit_code] = randomize.slice(0, random.int(10, 20))
+    })
+    console.log(origins)
+    console.log(destinations)
     run()
   } catch (e) { console.log(e) }
 }
 
 const run = () => {
-  console.log(origins)
   let svg = document.getElementById('svg-container')
   let _x = origins.map(d => d.latitude)
   let _y = origins.map(d => d.longitude)
@@ -33,31 +41,38 @@ const run = () => {
   let _xSorted = _x.sort((a,b) => a - b)
   let _ySorted = _y.sort((a,b) => a - b)
   // apply linearScales to our data to ensure that it fits our graph model
-  let linearScaleX = d3.scaleLinear()
+  let originScaleX = d3.scaleLinear()
                              .domain([_xSorted[0], _xSorted[_xSorted.length-1]])
-                             .range([lowerLimit, upperLimit])
-  let linearScaleY = d3.scaleLinear()
+                             .range([originLowerLimitX, originUpperLimitX])
+  let originScaleY = d3.scaleLinear()
                              .domain([_ySorted[0], _ySorted[_ySorted.length-1]])
-                             .range([lowerLimit, upperLimit])
+                             .range([originLowerLimitY, originUpperLimitY])
 
-  origins.forEach(d => {
+  // let olat = originScaleX(d.latitude)
+  // let olong = originScaleY(d.longitude)
+  let olats = origins.map(d => originScaleX(d.latitude))
+  let olongs = origins.map(d => originScaleY(d.longitude))
+  origins.forEach((d,i) => {
     svg.innerHTML += `
+      <!--
       <g>
         <circle cx=${w/2} cy=${w/2} r=${(w-4)/2} stroke='#000' fill='none'></circle>
       </g>
+      -->
       <g>
-        <rect x=${linearScaleX(d.latitude)} y=${linearScaleY(d.longitude)} width=${size} height=${size} fill='#000'></rect>
-        <text x=${linearScaleX(d.latitude) - size} y=${linearScaleY(d.longitude) - size} font-family='arial' font-size='0.5rem' fill='#000'>
+        <rect x=${olats[i]} y=${olongs[i]} width=${size} height=${size} fill='#000'></rect>
+        <text x=${olats[i] - size} y=${olongs[i] - size} font-family='arial' font-size='0.5rem' fill='#000'>
           ${d.code ? d.code : d.four_digit_code}
         </text>
         <!--
-        <text x=${linearScaleX(d.latitude)} y=${linearScaleY(d.longitude) - (size*.5)} font-family='arial' font-size='0.5rem' fill='#000'>
+        <text x=${olats[i]} y=${olongs[i] - (size*.5)} font-family='arial' font-size='0.5rem' fill='#000'>
           lat: ${parseFloat(d.latitude).toFixed(2)}, &nbsp; long:${parseFloat(d.longitude).toFixed(2)}
         </text>
         -->
       </g>
     `
   })
+
 
 }
 
